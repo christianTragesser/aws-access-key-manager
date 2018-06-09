82b6354345d7838c9c9e677f9dd9479872e844ba
Christian Tragesser <christian.tragesser@asynchrony.com>
2018-06-09 08:44:07 -0500

    remove mock module, no longer used

--- test/test_expireKeys.py
+++ test/test_expireKeys.py
@@ -1,7 +1,6 @@
 import pytest
 import datetime
 import sys, os
-import mock
 import botocore.session
 from botocore.stub import Stubber
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
