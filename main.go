27be422d8b2d9d354731cc478ffca59e7bc02d29
Christian Tragesser <christian.tragesser@wwt.com>
2021-09-22 09:37:34 -0500

    remove unused aspect

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"os"
-	"strings"
 
 	"github.com/christiantragesser/aws-access-key-manager/utils"
 	"github.com/sirupsen/logrus"
 )
-
-// var warnDays, _ = os.LookupEnv("WARN_DAYS")
-var warnDays int = 85
-var expireDays int = 90
-var userWhiteList string = ""
-var updateUserList []string = strings.Split(userWhiteList, ",")
 
 func main() {
 	slackURL, slackURL_exist := os.LookupEnv("SLACK_URL")
