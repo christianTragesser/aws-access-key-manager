71b403104bf2f74ae503395fbdfb550a7d057a45
Christian Tragesser <christian.tragesser@wwt.com>
2021-09-23 19:09:12 -0500

    provides slack notification client

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"os"
-
 	"github.com/christiantragesser/aws-access-key-manager/utils"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	slackURL, slackURL_exist := os.LookupEnv("SLACK_URL")
-	if !slackURL_exist {
-		logrus.Warn("Slack URL not set.")
-	} else {
-		logrus.Infof("Slack URL is %v", slackURL)
-	}
-
-	utils.ExamineKeys()
+	utils.SendSlackNotification("Test Message", "This is a test", "good")
 }
